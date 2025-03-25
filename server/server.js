0e2bbe7cab82b93851da565ebb49dbb7beced9b3
Alejandro Martinez <cloudof.alemar@gmail.com>
2025-03-25 13:14:51 -0700

    Placed cors middlware before routes.

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(routes);
 app.use(cors({
   origin: "https://alemar-habit-tracker.netlify.app",
   credentials: true
 }));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(routes);
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
