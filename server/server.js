f5ba5ad8326e38f40e4a4dfe9b29a3cfb3fb1bd4
Alejandro Martinez <cloudof.alemar@gmail.com>
2025-03-25 13:08:00 -0700

    Included cors package

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const db = require("./config/connection");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,10 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
+app.use(cors({
+  origin: "https://alemar-habit-tracker.netlify.app",
+  credentials: true
+}));
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
